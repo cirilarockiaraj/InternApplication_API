e5d362d30604e38d762ce532b80e99214320efa4
CIRIL AROCKIARAJ <CIRIL AROCKIARAJ@CIRILAROCKIARAJ>
2023-11-11 20:24:44 +0530

    New Project Created

---
+++ src/main/java/com/app/InternAPI/controller/InternController.java
@@ -0,0 +1,21 @@
+package com.app.InternAPI.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dashboard")
+public class InternController {
+	
+	@GetMapping("/allinterns")
+	public String getInterns() {
+		return "Hello Interns";
+	}
+	
+	@GetMapping("/intern/{name}/{id}")
+	public String getOneInterns(@PathVariable(value = "name") String name, @PathVariable(value = "id") String id) {
+		return "Hi "+name+" your ID : "+id;
+	}
+}
---
+++ src/test/java/com/app/InternAPI/InternApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.InternAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
